46406cfc3f562731f7026abf173f2aa09547d291
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2024-08-19 09:28:26 -0400

    Added verbage to about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,7 +4,19 @@ export default function AboutPage() {
   return (
     <div className={styles.layout}>
       <h1 className={styles.aboutTitle}>About</h1>
-      <p className={styles.aboutBody}>Verbage Here</p>
+      <p className={styles.aboutBody}>
+        Sometimes making your special day come together can be overwhelming,
+        that's where I come in. This is your one-stop shop for all things
+        wedding planning. Whether you need day-of, month-of, or even total
+        wedding planning, I'm here for you on your special day. I spent several
+        years working at luxury brands like La Mer and Burberry as an executive
+        assistant, planning work parties, events, and travel. However, I fell in
+        love with wedding planning when planning my own wedding.
+      </p>
+      <p className={styles.aboutBody}>
+        Now I want to bring all the expertise I've gained to your wedding,
+        making sure everything goes smoothly.
+      </p>
     </div>
   );
 }
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,6 +4,10 @@ export default function HomePage() {
   return (
     <div className={styles.layout}>
       <h1 className={styles.homeTitle}>Making your special day a breeze</h1>
+      <p className={styles.homeSubTitle}>
+        We are here to help your day go as smoothly as possible. We take the
+        stress out of your special day allowing you to fully enjoy the moment.
+      </p>
     </div>
   );
 }
