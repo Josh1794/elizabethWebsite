33f3cf83d15183c4cae952d06fe814f6c4f0abb3
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2024-08-19 09:38:20 -0400

    added contact email and modified styling

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,8 +3,16 @@ import styles from './contact.module.css';
 export default function ContactPage() {
   return (
     <div className={styles.layout}>
-      <h1 className={styles.aboutTitle}>Contact</h1>
-      <p className={styles.aboutBody}>Verbage Here</p>
+      <h1 className={styles.contactTitle}>Contact</h1>
+      <div className={styles.contactSection}>
+        <p className={styles.contactBody}>Email:</p>
+        <a
+          className={styles.contactBodyLink}
+          href='mailto: elizabeth.treimanis.events@gmail.com'
+        >
+          elizabeth.treimanis.events@gmail.com
+        </a>
+      </div>
     </div>
   );
 }
